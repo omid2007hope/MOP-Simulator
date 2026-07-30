6fceb98280f66b0a8dc8bce550f442551ef53351
omid2007hope <omidhope2007@gmail.com>
2026-07-30 21:59:53 +0200

    feat: implement ImpactSimulator with rigid body and hydrodynamic penetration models

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -352,7 +352,7 @@ void ImpactSimulator::generateHtml3DVisualizer(const std::vector<SimulationResul
     replaceAll(html, "{{PROJ_YIELD_STRENGTH}}", std::to_string(proj.yield_strength));
     replaceAll(html, "{{TARGET_NAME}}", target.name);
     replaceAll(html, "{{TARGET_DENSITY}}", std::to_string(target.density));
-    replaceAll(html, "{{TARGET_BEARING_STRENGTH}}", std::to_string(100.0e6));
+    replaceAll(html, "{{TARGET_BEARING_STRENGTH}}", std::to_string(cons.compressiveStrengthOfUHPC));
     replaceAll(html, "{{DRAG_COEFFICIENT}}", std::to_string(1.2));
     replaceAll(html, "{{SPEED_OF_SOUND}}", std::to_string(cons.SPEED_OF_SOUND));
 
