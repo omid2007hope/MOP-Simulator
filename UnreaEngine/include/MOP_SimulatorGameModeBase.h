7a028daa9b2251bdddf6021bf691fc5305cc941c
omid2007hope <omidhope2007@gmail.com>
2026-08-11 07:31:34 +0200

    feat: initialize MOP_SimulatorGameModeBase class for project setup

---
+++ UnreaEngine/include/MOP_SimulatorGameModeBase.h
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MOP_SimulatorGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOP_SIMULATOR_API AMOP_SimulatorGameModeBase : public AGameModeBase {
+	GENERATED_BODY()
+};
