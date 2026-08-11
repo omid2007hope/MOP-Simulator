0f08f7d50e6276c840a7e70e026f9ed0ef9f1e75
omid2007hope <omidhope2007@gmail.com>
2026-08-11 07:18:14 +0200

    feat: implement Projectile and Environment actor classes with associated physics data structures

--- UnreaEngine/include/Projectile.h
+++ UnreaEngine/include/Projectile.h
@@ -2,13 +2,69 @@
 
 #pragma once
 
-#include <numbers>
-
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-#include "Target.generated.h"
+#include "Projectile.generated.h"
 
 
+USTRUCT(BlueprintType)
+struct FProjectile {
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Properties")
+	FString Name = TEXT("");
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Properties")
+	double Length = 0.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Properties")
+	double Diameter = 0.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Properties")
+	double CurvatureNoseReduce = 0.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Properties")
+	double TotalMass = 0.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Properties")
+	double ExplosiveMass = 0.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Properties")
+	double CasingDensity = 0.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Properties")
+	double YieldStrength = 0.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Properties")
+	double AreaMomentInertia = 0.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Properties")
+	double ElasticModulus = 0.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Properties")
+	double CasingWallThickness = 0.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Properties")
+	double HugoniotC0 = 0.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Properties")
+	double HugoniotS = 0.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Properties")
+	double ExplosiveCriticalEnergy = 0.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Properties")
+	double ExplosiveEnergyJPerKg = 0.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Properties")
+	double SpecificHeat = 460.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Properties")
+	double MeltingPoint = 1800.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Properties")
+	double HeatOfFusion = 272000.0;
+};
 
 UCLASS()
 class AProjectile : public AActor {
@@ -22,8 +78,9 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Configuration")
-	FProjectile ProjectilePhysicsData;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<class UStaticMeshComponent> ProjectileMesh;
 
 public:
 	// Called every frame
--- UnreaEngine/src/Projctile.cpp
+++ UnreaEngine/src/Projctile.cpp
@@ -7,7 +7,13 @@
 AProjectile::AProjectile() {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+
+	// assign a mesh
+	ProjectileMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ProjectileMesh"));
+	RootComponent = ProjectileMesh;
 }
+
 // Called when the game starts or when spawned
 void AProjectile::BeginPlay() {
 	Super::BeginPlay();
