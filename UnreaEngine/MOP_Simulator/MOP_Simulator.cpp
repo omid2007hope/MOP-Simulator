5fea133d4f5a751f95b6239882d1c2fda52b057f
omid2007hope <omidhope2007@gmail.com>
2026-08-11 12:01:06 +0200

    feat: implement Target actor and data structures for projectile simulation in Unreal Engine

--- UnreaEngine/MOP_Simulator/MOP_Simulator.cpp
+++ UnreaEngine/MOP_Simulator/MOP_Simulator.cpp
@@ -1,6 +1,96 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "MOP_Simulator.h"
+
 #include "Modules/ModuleManager.h"
 
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MOP_Simulator, "MOP_Simulator" );
+
+IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, MOP_Simulator, "MOP_Simulator");
+
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Target.h"
+
+// Sets default values
+ATarget::ATarget() {
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// assign a mesh
+	TargetMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TargetMesh"));
+	RootComponent = TargetMesh; // Make the mesh the root of the entire Actor!
+
+	// Set Default Target Data (Fordow-like Hardened Mountain Facility)
+	TargetPhysicsData.Name = TEXT("Fordow-like Hardened Mountain Facility");
+
+	// Layer 1: Soil/Overburden
+	FTargetLayer SoilLayer;
+	SoilLayer.MaterialName = TEXT("Soil/Overburden + Surface Cap");
+	SoilLayer.Thickness = 3.0;
+	SoilLayer.Density = 1800.0;
+	SoilLayer.CompressiveStrength = 20.0e6;
+	SoilLayer.HugoniotC0 = 1800.0;
+	SoilLayer.HugoniotS = 1.5;
+	TargetPhysicsData.Layers.Add(SoilLayer);
+
+	// Layer 2: Reinforced Concrete
+	FTargetLayer ConcreteLayer;
+	ConcreteLayer.MaterialName =
+		TEXT("Reinforced Concrete (facility structure / shaft lining)");
+	ConcreteLayer.Thickness = 5.0;
+	ConcreteLayer.Density = 2650.0;
+	ConcreteLayer.CompressiveStrength = 70.0e6;
+	ConcreteLayer.RebarVolumeFraction = 0.04;
+	ConcreteLayer.RebarYieldStrength = 500.0e6;
+	ConcreteLayer.HugoniotC0 = 3200.0;
+	ConcreteLayer.HugoniotS = 1.9;
+	TargetPhysicsData.Layers.Add(ConcreteLayer);
+
+	// Layer 3: Solid Mountain Rock
+	FTargetLayer RockLayer;
+	RockLayer.MaterialName = TEXT("Solid Mountain Rock (limestone/granite-like)");
+	RockLayer.Thickness = 90.0;
+	RockLayer.Density = 2750.0;
+	RockLayer.CompressiveStrength = 170.0e6;
+	RockLayer.HugoniotC0 = 3800.0;
+	RockLayer.HugoniotS = 1.35;
+	TargetPhysicsData.Layers.Add(RockLayer);
+
+	// Trigger after impact
+	TargetMesh->OnComponentHit.AddDynamic(this, &ATarget::OnTargetHit);
+}
+
+// Impact
+void ATarget::ReceiveImpact() {};
+// on Impact Event
+void ATarget::OnTargetHit(UPrimitiveComponent* HitComponent,
+			  AActor* OtherActor,
+			  UPrimitiveComponent* OtherComp,
+			  FVector NormalImpulse,
+			  const FHitResult& Hit) {
+	// 1. Check if the thing that hit us is actually the Projectile!
+	AProjectile* HittingBomb = Cast<AProjectile>(OtherActor);
+
+	if (HittingBomb != nullptr) {
+
+		// Receive bomb's data
+		FProjectile BombData = HittingBomb->GetPhysicsData();
+
+		// IT IS THE BOMB!
+		// 2. THIS is where you paste your MASSIVE while-loop from `simulation.cpp`!
+		// You will calculate the penetration depth using `TargetPhysicsData.Layers`
+		// and the bomb's mass!
+	}
+}
+
+
+// Called when the game starts or when spawned
+void ATarget::BeginPlay() {
+	Super::BeginPlay();
+}
+
+// Called every frame
+void ATarget::Tick(float DeltaTime) {
+	Super::Tick(DeltaTime);
+}
--- UnreaEngine/MOP_Simulator/MOP_Simulator.h
+++ UnreaEngine/MOP_Simulator/MOP_Simulator.h
@@ -4,3 +4,174 @@
 
 #include "CoreMinimal.h"
 
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include <numbers>
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Target.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct FProjectile {
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Properties")
+	FString Name = TEXT("");
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Properties")
+	double Length = 0.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Properties")
+	double Diameter = 0.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Properties")
+	double CurvatureNoseReduce = 0.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Properties")
+	double TotalMass = 0.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Properties")
+	double ExplosiveMass = 0.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Properties")
+	double CasingDensity = 0.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Properties")
+	double YieldStrength = 0.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Properties")
+	double AreaMomentInertia = 0.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Properties")
+	double ElasticModulus = 0.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Properties")
+	double CasingWallThickness = 0.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Properties")
+	double HugoniotC0 = 0.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Properties")
+	double HugoniotS = 0.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Properties")
+	double ExplosiveCriticalEnergy = 0.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Properties")
+	double ExplosiveEnergyJPerKg = 0.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Properties")
+	double SpecificHeat = 460.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Properties")
+	double MeltingPoint = 1800.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Properties")
+	double HeatOfFusion = 272000.0;
+};
+
+
+// Target layer specification
+USTRUCT(BlueprintType)
+struct FTargetLayer {
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Target Properties")
+	FString MaterialName = TEXT("");
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Target Properties")
+	double Thickness = 0.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Target Properties")
+	double Density = 0.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Target Properties")
+	double CompressiveStrength = 0.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Target Properties")
+	double RebarVolumeFraction = 0.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Target Properties")
+	double RebarYieldStrength = 0.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Target Properties")
+	double PulverizedDepth = 0.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Target Properties")
+	double HugoniotC0 = 0.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Target Properties")
+	double HugoniotS = 0.0;
+};
+
+// Target material specification
+USTRUCT(BlueprintType)
+struct FTarget {
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Target Properties")
+	FString Name = TEXT("");
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Target Properties")
+	TArray<FTargetLayer> Layers;
+
+	void PulverizeDepth(double BreachDepth) {
+		double CurrentDepthAcc = 0.0;
+		for (FTargetLayer& Layer : Layers) {
+			if (BreachDepth > CurrentDepthAcc) {
+				Layer.PulverizedDepth = FMath::Max(
+					Layer.PulverizedDepth,
+					FMath::Min(Layer.Thickness, BreachDepth - CurrentDepthAcc));
+			}
+			CurrentDepthAcc += Layer.Thickness;
+		}
+	}
+};
+
+UCLASS()
+class ATarget : public AActor {
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ATarget();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Target Configuration")
+	FTarget TargetPhysicsData;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<class UStaticMeshComponent> TargetMesh;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Configuration")
+	FProjectile ProjectilePhysicsData;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<class UStaticMeshComponent> ProjectileMesh;
+
+
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+	void OnTargetHit(UPrimitiveComponent* HitComponent,
+			 AActor* OtherActor,
+			 UPrimitiveComponent* OtherComp,
+			 FVector NormalImpulse,
+			 const FHitResult& Hit);
+
+	// Bomb's data
+	FProjectile GetPhysicsData() const {
+		return ProjectilePhysicsData;
+	}
+};
