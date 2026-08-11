8ecdb86f0ed1df5869700cd9ed6736bd2f8eb3ea
omid2007hope <omidhope2007@gmail.com>
2026-08-11 07:21:03 +0200

    feat: implement base Environment class for simulation management

--- UnreaEngine/include/Env.h
+++ UnreaEngine/include/Env.h
@@ -2,30 +2,13 @@
 
 #pragma once
 
-#include <numbers>
-
 #include "CoreMinimal.h"
-#include "GameFramework/Actor.h"
-#include "Target.generated.h"
 
-
-
-UCLASS()
-class AEnviornment : public AActor {
-	GENERATED_BODY()
-
+/**
+ * 
+ */
+class Environment {
 public:
-	// Sets default values for this actor's properties
-	AEnviornment();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enviornment Configuration")
-	FEnviornment EnviornmentPhysicsData;
-
-public:
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	Environment();
+	~Environment();
 };
--- UnreaEngine/src/Env.cpp
+++ UnreaEngine/src/Env.cpp
@@ -1,19 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Enviornment.h"
+#include "Environment.h"
 
-// Sets default values
-AEnviornment::AEnviornment() {
-	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-}
-// Called when the game starts or when spawned
-void AEnviornment::BeginPlay() {
-	Super::BeginPlay();
-}
+Environment::Environment() {}
 
-// Called every frame
-void AEnviornment::Tick(float DeltaTime) {
-	Super::Tick(DeltaTime);
-}
+Environment::~Environment() {}
