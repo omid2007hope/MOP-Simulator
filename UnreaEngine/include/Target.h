f1ae3ff44db47286c07e2f293ecfefe0bbb1bb7a
omid2007hope <omidhope2007@gmail.com>
2026-08-11 05:59:29 +0200

    feat: add Target and TargetLayer structs for physical material simulation and actor definition

--- UnreaEngine/include/Target.h
+++ UnreaEngine/include/Target.h
@@ -2,9 +2,44 @@
 
 #pragma once
 
+#include <numbers>
+#include <string>
+#include <vector>
+
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "Target.generated.h"
+
+
+// Target layer specification
+struct TargetLayer {
+	std::string material_name;
+	double thickness = 0.0;
+	double density = 0.0;
+	double compressive_strength = 0.0;
+	double rebar_volume_fraction = 0.0;
+	double rebar_yield_strength = 0.0;
+	double pulverized_depth = 0.0;
+	double hugoniot_c0 = 0.0;
+	double hugoniot_s = 0.0;
+};
+
+// Target material specification
+struct Target {
+	std::string name;
+	std::vector<TargetLayer> layers;
+	void pulverizeDepth(double breachDepth) {
+		double currentDepthAcc = 0.0;
+		for (auto& layer : layers) {
+			if (breachDepth > currentDepthAcc) {
+				layer.pulverized_depth = std::max(
+					layer.pulverized_depth,
+					std::min(layer.thickness, breachDepth - currentDepthAcc));
+			}
+			currentDepthAcc += layer.thickness;
+		}
+	}
+};
 
 UCLASS()
 class ATarget : public AActor {
