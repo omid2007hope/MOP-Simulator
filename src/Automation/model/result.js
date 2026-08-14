0622c7149e3d2eee38992c432dc8b707f5618156
omid2007hope <omidhope2007@gmail.com>
2026-08-14 12:14:51 +0200

    feat: add simulation result Mongoose model and configure Prettier formatting rules

--- src/Automation/model/result.js
+++ src/Automation/model/result.js
@@ -1,1 +1,8 @@
-import "";
+// © 2026 Omid Teimory. All rights reserved.
+// Signature: OmidTeimory-2026
+
+const mongoose = require('mongoose');
+
+const simulationResult = new mongoose.Schema({});
+
+module.exports = mongoose.model('SimulationResult', simulationResult);
