ccb99e56f74134f6bc50b7deee884c7cc015434f
omid2007hope <omidhope2007@gmail.com>
2026-08-21 01:55:11 +0200

    feat: initialize home page components and layout structure with persistent header

---
+++ frontend/src/app/components/layout/header.jsx
@@ -0,0 +1,15 @@
+export default function Header() {
+  return (
+    <header className="flex flex-row w-full h-9/100 bg-blue-900 border-b-2 border-black z-250 fixed">
+      <div className="h-full w-4/100 flex flex-col justify-around items-center text-white font-bold border">
+        each
+      </div>
+      <div className="h-full w-36/100 flex flex-col justify-around items-center text-white font-bold border">
+        each
+      </div>
+      <div className="h-full w-60/100 flex flex-col justify-around items-center text-white font-bold border">
+        each
+      </div>
+    </header>
+  );
+}
