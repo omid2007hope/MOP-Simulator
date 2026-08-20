ca27fe487ac2a88783729f68c1d9c6ec047a380b
omid2007hope <omidhope2007@gmail.com>
2026-08-21 01:57:46 +0200

    feat: create Header component with logo, title, and search bar sections

--- frontend/src/app/components/layout/header.jsx
+++ frontend/src/app/components/layout/header.jsx
@@ -1,14 +1,18 @@
 export default function Header() {
   return (
     <header className="flex flex-row w-full h-9/100 bg-blue-900 border-b-2 border-black z-250 fixed">
+      {/* logo */}
       <div className="h-full w-4/100 flex flex-col justify-around items-center text-white font-bold border">
-        each
+        <img src="" alt="" />
       </div>
+      {/* title */}
       <div className="h-full w-36/100 flex flex-col justify-around items-center text-white font-bold border">
-        each
+        <h1 className=""></h1>
       </div>
+      {/* search bar  */}
       <div className="h-full w-60/100 flex flex-col justify-around items-center text-white font-bold border">
-        each
+        <button className=""></button>
+        <input className=" " type="text" />
       </div>
     </header>
   );
