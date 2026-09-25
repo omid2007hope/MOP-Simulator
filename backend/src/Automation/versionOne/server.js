faa0dd53cfc6921d1b3b5775fb53e3b6501b6ddb
omid2007hope <omidhope2007@gmail.com>
2026-09-25 12:22:44 +0200

    feat: add version 1 server and HTML visualizer templates

--- backend/src/Automation/versionOne/server.js
+++ backend/src/Automation/versionOne/server.js
@@ -30,6 +30,9 @@ const researchRouter = require('./router/research');
 const simulationDataRouter = require('./router/simulationData');
 const pipelineRouter = require('./router/pipeline');
 const { coreHealth } = require('../../AI/versionOne/core');
+\A
+// Serve static files from the root of the backend directory (e.g., for 3d_visualizer.html)
+app.use(express.static(path.join(__dirname, '../../..')));
 
 app.use(healthRouter);
 app.use(researchRouter);
